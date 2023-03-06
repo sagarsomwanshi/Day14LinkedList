1d703a6ba2216940c08208b5684b4eb904688426
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 19:23:18 +0530

    Added : Add first method

--- src/LinkList.java
+++ src/LinkList.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class LinkList {
     public static void main(String[] args) {
         LinkedList<Integer> link = new LinkedList();
-        link.add(56);
-        link.add(30);
-        link.add(70);
-        System.out.println("Linked list : "+link);
+        link.addFirst(70);
+        link.addFirst(30);
+        link.addFirst(56);
 
+        System.out.println("Linked list : "+link);
 
     }
 }
