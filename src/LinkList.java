df9101e22282664b4a9246b18427f63f300d5acb
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 19:35:25 +0530

    Added : Insertion method

--- src/LinkList.java
+++ src/LinkList.java
@@ -3,9 +3,9 @@ public class LinkList {
     public static void main(String[] args) {
         LinkedList<Integer> link = new LinkedList();
         link.add(56);
-        link.addLast(30);
-        link.addLast(70);
-
+        link.add(70);
+        System.out.println("link list before inserting 30 : "+link);
+        link.add(1,30);
         System.out.println("Linked list : "+link);
 
     }
