4d04760857b4d4793ba3f28f2daa730a99a1e840
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 21:31:28 +0530

    Added : searchs in linked list & written junit test

--- src/LinkList.java
+++ src/LinkList.java
@@ -1,13 +1,45 @@
+import org.junit.Test;
+
 import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class LinkList {
+
+    LinkedList<Integer> link = new LinkedList();
+
+
     public static void main(String[] args) {
-        LinkedList<Integer> link = new LinkedList();
-        link.add(56);
-        link.add(30);
-        link.add(70);
-        System.out.println("link list before deleting 70 : "+link);
-        link.removeLast();
-        System.out.println("Linked list : "+link);
+
+        LinkList ll = new LinkList();
+        ll.link.add(56);
+        ll.link.add(30);
+        ll.link.add(70);
+
+            int check=0;
+        for(int i = 0; i<ll.link.size();i++){
+            if(ll.link.get(i) == 30){
+
+                System.out.println("link list contains 30");
+                check = 1;
+            }
+
+        }
+        if(check != 1){
+            System.out.println("link list dose not contain 30");
+        }
+
 
     }
+    public boolean checkThirty(LinkedList<Integer> l){
+        return l.contains(30);
+
+    }
+    @Test
+    public void checkNode(){
+        link.add(30);
+        assertEquals(true, checkThirty(link));
+
+    }
+
 }
