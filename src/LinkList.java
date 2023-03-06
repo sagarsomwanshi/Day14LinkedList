3620c02636b36b63e4863787b56677419582d184
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 18:40:27 +0530

    Added : Simple Linked List Created

---
+++ src/LinkList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class LinkList {
+    public static void main(String[] args) {
+        LinkedList<Integer> link = new LinkedList();
+        link.add(56);
+        link.add(30);
+        link.add(70);
+        System.out.println("Linked list : "+link);
+
+
+    }
+}
