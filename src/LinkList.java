ef055260d1818baddb5d7bfc4b42899a83045f8c
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-03-06 19:38:18 +0530

    Added : 1st element deleted

--- src/LinkList.java
+++ src/LinkList.java
@@ -3,9 +3,10 @@ public class LinkList {
     public static void main(String[] args) {
         LinkedList<Integer> link = new LinkedList();
         link.add(56);
+        link.add(30);
         link.add(70);
-        System.out.println("link list before inserting 30 : "+link);
-        link.add(1,30);
+        System.out.println("link list before deleting 56 : "+link);
+        link.removeFirst();
         System.out.println("Linked list : "+link);
 
     }
